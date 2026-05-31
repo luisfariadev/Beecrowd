bbc13e5e5b09b10502607a9dcf4e137f460b4bee
Luis Fernando <luisfaria.dev@gmail.com>
2026-05-31 11:02:50 -0300

    Solução para o exercício 1188

---
+++ 1188.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char caractere;
+    scanf("%c", &caractere);
+    double matriz[12][12];
+    double soma = 0;
+    int cont = 0;
+
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            scanf("%lf", &matriz[i][j]);
+
+            if (i > j && i + j > 11)
+            {
+                soma = soma + matriz[i][j];
+                cont = cont + 1;
+            }
+        }
+    }
+
+    if (caractere == 'M')
+    {
+        soma = soma / cont;
+    }
+
+    printf("%.1lf\n", soma);
+
+    return 0;
+}
