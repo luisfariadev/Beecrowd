b2895bc561d75f4003cc6d904e1480cdada8355d
Luis Fernando <luisfaria.dev@gmail.com>
2026-05-31 11:45:15 -0300

    Solução para o exercício 1046

---
+++ 1046.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int inicio, fim, duracao;
+
+    scanf("%d %d", &inicio, &fim);
+
+    if (inicio < fim)
+    {
+        duracao = fim - inicio;
+    }
+    else if (inicio > fim)
+    {
+        duracao = 24 - inicio + fim;
+    }
+    else
+    { // inicio == fim
+        duracao = 24;
+    }
+
+    printf("O JOGO DUROU %d HORA(S)\n", duracao);
+
+    return 0;
+}
