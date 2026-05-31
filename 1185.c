e57224b608072c86c26704bdad8c7d197698ef6c
Luis Fernando <luisfaria.dev@gmail.com>
2026-05-31 11:05:00 -0300

    Solução para o exercício 1185

---
+++ 1185.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char caractere;
+    scanf("%c", &caractere);
+    float matriz[12][12];
+    float soma = 0;
+    int quantidade = 0;
+
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            scanf("%f", &matriz[i][j]);
+
+            if (i + j < 11)
+            {
+                soma = soma + matriz[i][j];
+                quantidade = quantidade + 1;
+            }
+        }
+    }
+
+    if (caractere == 'M')
+    {
+        soma = soma / quantidade;
+    }
+
+    printf("%.1f\n", soma);
+
+    return 0;
+}
