888df6a383212359fb83b56d80ec6be359c99bfb
Luis Fernando <luisfaria.dev@gmail.com>
2026-05-31 10:59:22 -0300

    Solução para o exercício 1759

---
+++ 1759.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i == n - 1)
+        {
+            printf("Ho!\n");
+        }
+        else
+        {
+            printf("Ho ");
+        }
+    }
+
+    return 0;
+}
