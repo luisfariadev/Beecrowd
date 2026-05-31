75ce2573ae6f5f158c5484a85b00f4ccc4aefd5f
Luis Fernando <luisfaria.dev@gmail.com>
2026-05-31 11:34:40 -0300

    Solução para o exercício 1114

---
+++ 1114.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n, s;
+    scanf("%d", &n);
+
+    while (n != 2002)
+    {
+        printf("Senha Invalida\n");
+        scanf("%d", &n);
+    }
+
+    printf("Acesso Permitido\n");
+
+    return 0;
+}
