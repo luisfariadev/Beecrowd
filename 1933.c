e2a79f2a50bb5c9327a83ed739d59ff881139e9c
Luis Fernando <luisfaria.dev@gmail.com>
2026-05-31 10:41:09 -0300

    Solução para o exercício 1933

---
+++ 1933.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    if (n > m)
+    {
+        printf("%d\n", n);
+    }
+    else
+    {
+        printf("%d\n", m);
+    }
+
+    return 0;
+}
