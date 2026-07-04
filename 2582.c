21adad45f1834594cf8d6a304de2218de586c327
Luis Fernando <luisfaria.dev@gmail.com>
2026-07-04 10:04:46 -0300

    Solução para o exercício 2582

---
+++ 2582.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, x, y;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d %d", &x, &y);
+
+        if (x + y == 0)
+        {
+            printf("PROXYCITY\n");
+        }
+        else if (x + y == 1)
+        {
+            printf("P.Y.N.G.\n");
+        }
+        else if (x + y == 2)
+        {
+            printf("DNSUEY!\n");
+        }
+        else if (x + y == 3)
+        {
+            printf("SERVERS\n");
+        }
+        else if (x + y == 4)
+        {
+            printf("HOST!\n");
+        }
+        else if (x + y == 5)
+        {
+            printf("CRIPTONIZE\n");
+        }
+        else if (x + y == 6)
+        {
+            printf("OFFLINE DAY\n");
+        }
+        else if (x + y == 7)
+        {
+            printf("SALT\n");
+        }
+        else if (x + y == 8)
+        {
+            printf("ANSWER!\n");
+        }
+        else if (x + y == 9)
+        {
+            printf("RAR?\n");
+        }
+        else
+        {
+            printf("WIFI ANTENNAS\n");
+        }
+    }
+
+    return 0;
+}
