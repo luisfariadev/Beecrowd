4737b93e59fd2c408979a9bd5dd784844d619fdb
Luis Fernando <luisfaria.dev@gmail.com>
2026-07-13 12:43:01 -0300

    Solução para o exercício 2747

---
+++ 2747.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i < 39; i++)
+    {
+        printf("-");
+    }
+
+    printf("\n");
+
+    for (int j = 0; j < 5; j++)
+    {
+        for (int i = 0; i < 39; i++)
+        {
+            if (i == 0 || i == 38)
+            {
+                printf("|");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        printf("\n");
+    }
+
+    for (int i = 0; i < 39; i++)
+    {
+        printf("-");
+    }
+
+    printf("\n");
+
+    return 0;
+}
