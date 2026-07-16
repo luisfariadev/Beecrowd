ed8c284f407316bb703e59064c03179753f48316
Luis Fernando <luisfaria.dev@gmail.com>
2026-07-16 19:15:52 -0300

    Solução para o exercício 2861

---
+++ 2861.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        char pergunta[200];
+        scanf("%s", &pergunta);
+        printf("gzuz\n");
+    }
+
+    return 0;
+}
