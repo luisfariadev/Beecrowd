a314b12e77fa642dcbb1233c53f70ec21d86aed1
Luis Fernando <luisfaria.dev@gmail.com>
2026-07-30 10:23:22 -0300

    Solução para o exercício 3157

---
+++ 3157.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char p;
+
+    while (scanf("%c", &p) == 1 && p != '\n')
+    {
+    }
+
+    printf("Nao se refuta ciencia com opiniao\n");
+
+    return 0;
+}
