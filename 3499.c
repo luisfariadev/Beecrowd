6263f22434af3422591534bf76e31af44184eb73
Luis Fernando <luisfaria.dev@gmail.com>
2026-08-01 20:37:13 -0300

    Solução para o exercício 3499

---
+++ 3499.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    if (n > 3)
+    {
+        printf("vai ficar sem o biscoito\n");
+    }
+    else
+    {
+        printf("vai ganhar o biscoito\n");
+    }
+
+    return 0;
+}
