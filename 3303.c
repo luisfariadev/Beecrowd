99594559a21e5ed4926e2bfb45e1565ca8a9baf2
Luis Fernando <luisfaria.dev@gmail.com>
2026-08-01 20:50:07 -0300

    Solução para o exercício 3303

---
+++ 3303.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char palavra[25];
+    scanf("%s", palavra);
+
+    if (strlen(palavra) >= 10)
+    {
+        printf("palavrao\n");
+    }
+    else
+    {
+        printf("palavrinha\n");
+    }
+
+    return 0;
+}
