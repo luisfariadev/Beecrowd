0d1c73171b01face6bc28f42d84f024364a0a4af
Luis Fernando <luisfaria.dev@gmail.com>
2026-08-01 20:03:49 -0300

    Solução para o exercício 3493

---
+++ 3493.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if (b > a)
+    {
+        printf("ok\n");
+    }
+    else
+    {
+        printf("no\n");
+    }
+
+    return 0;
+}
