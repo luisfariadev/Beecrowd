d5ec8d244a96c6105a74d781a874e060cc3a6dba
Luis Fernando <luisfaria.dev@gmail.com>
2026-09-17 12:24:33 -0300

    Solução para o exercício 1763

---
+++ 1763.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map<string, string> mapa =
+        {
+            {"brasil", "Feliz Natal!"},
+            {"alemanha", "Frohliche Weihnachten!"},
+            {"austria", "Frohe Weihnacht!"},
+            {"coreia", "Chuk Sung Tan!"},
+            {"espanha", "Feliz Navidad!"},
+            {"grecia", "Kala Christougena!"},
+            {"estados-unidos", "Merry Christmas!"},
+            {"inglaterra", "Merry Christmas!"},
+            {"australia", "Merry Christmas!"},
+            {"portugal", "Feliz Natal!"},
+            {"suecia", "God Jul!"},
+            {"turquia", "Mutlu Noeller"},
+            {"argentina", "Feliz Navidad!"},
+            {"chile", "Feliz Navidad!"},
+            {"mexico", "Feliz Navidad!"},
+            {"antardida", "Merry Christmas!"},
+            {"canada", "Merry Christmas!"},
+            {"irlanda", "Nollaig Shona Dhuit!"},
+            {"belgica", "Zalig Kerstfeest!"},
+            {"italia", "Buon Natale!"},
+            {"libia", "Buon Natale!"},
+            {"siria", "Milad Mubarak!"},
+            {"marrocos", "Milad Mubarak!"},
+            {"japao", "Merii Kurisumasu!"}};
+
+    string analise;
+
+    while (cin >> analise)
+    {
+        auto it = mapa.find(analise);
+
+        if (it == mapa.end())
+        {
+            cout << "--- NOT FOUND ---" << endl;
+        }
+        else
+        {
+            cout << it->second << endl;
+        }
+    }
+
+    return 0;
+}
